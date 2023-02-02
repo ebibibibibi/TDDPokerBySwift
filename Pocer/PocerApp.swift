7490e899d4c3b50360b13e6b6bc3fdfedbabec7f
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2023-02-02 20:00:16 +0900

    First commit

---
+++ Pocer/PocerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocerApp.swift
+//  Pocer
+//
+//  Created by kotomi tahkahashi on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct PocerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
